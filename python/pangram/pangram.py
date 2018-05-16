01c78bf3e155c931b50f7517ad396c478a27ccb6
Tha-Fox <kehakettu@protonmail.com>
2018-05-17 00:35:21 +0300

    Second iteration for pangram.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,6 +1,3 @@
 from string import ascii_lowercase
 def is_pangram(sentence):
-    for character in ascii_lowercase:
-        if character not in sentence.lower():
-            return False
-    return True
+    return set(ascii_lowercase).issubset(set(sentence.lower()))
